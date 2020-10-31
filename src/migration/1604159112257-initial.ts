a65b4e515cc9bd78e9971ea1cf95a399d8a2135e
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-10-31 15:50:28 +0000

    fix: migration files sintetic

--- src/migration/1604159112257-initial.ts
+++ src/migration/1604159112257-initial.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class InitialProject1600816525908 implements MigrationInterface {
-    name = 'InitialProject1600816525908'
+export class initial1604159112257 implements MigrationInterface {
+    name = 'initial1604159112257'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `users` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NULL DEFAULT '', `last_name` varchar(255) NULL DEFAULT '', `email` varchar(255) NOT NULL, `password` varchar(128) NOT NULL, `roles` text NOT NULL, `status` tinyint NOT NULL DEFAULT 1, `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (`id`)) ENGINE=InnoDB");
-        await queryRunner.query("CREATE TABLE `posts` (`id` int NOT NULL AUTO_INCREMENT, `slug` text NOT NULL, `title` varchar(150) NOT NULL, `excerpt` varchar(255) NOT NULL, `content` text NOT NULL, `category` varchar(100) NULL, `tags` text NOT NULL, `status` tinyint NOT NULL DEFAULT 1, `createdAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `author` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `posts` (`id` int NOT NULL AUTO_INCREMENT, `slug` text NOT NULL, `title` varchar(150) NOT NULL, `excerpt` varchar(255) NOT NULL, `content` text NOT NULL, `category` varchar(100) NULL, `tags` text NOT NULL, `status` tinyint NOT NULL DEFAULT 1, `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `author` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
         await queryRunner.query("ALTER TABLE `posts` ADD CONSTRAINT `FK_d03fb91772937997f010466a007` FOREIGN KEY (`author`) REFERENCES `users`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
     }
 
