4c7654ec29f2eda4fa0a8511667cb20f1c82f40e
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-11-01 02:26:17 +0000

    refact: config mailer options

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -1,18 +1,6 @@
 import { registerAs } from '@nestjs/config';
 import { MailerOptions } from '@nestjs-modules/mailer';
-import * as nodemailer from 'nodemailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-
-// export const gmailTransporter = nodemailer.createTransport(
-//   {
-//     service: 'gmail',
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   },
-//   console.log(process.env.NODE_ENV),
-// );
 
 const mailerModuleOptions = (): MailerOptions => {
   return {
@@ -24,7 +12,7 @@ const mailerModuleOptions = (): MailerOptions => {
       },
     },
     defaults: {
-      from: `"${process.env.EMAIL_DEFAULT_FROM_NAME}" <${process.env.EMAIL_FROM_EMAIL}>`,
+      from: `"${process.env.EMAIL_DEFAULT_FROM_NAME}" <${process.env.EMAIL_DEFAULT_FROM_EMAIL}>`,
     },
     template: {
       dir: process.cwd() + '/template/layouts',
